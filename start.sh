eecef0ab93bf172ef3dd27b8e8c4e889c62f866f
n-p-z <123107529+n-p-z@users.noreply.github.com>
2024-09-22 12:48:48 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,29 +1,6 @@
 #!/bin/sh
-if [ "${CONFIG}x" == "x" ]; then
-	CONFIG=/data/config.ini
-fi
-
-if [ ! -e $CONFIG ]; then
-	cp /config.ini /data
-fi
-
-
-brctl addbr virbr0
-ip link set dev virbr0 up
-if [ "${BRIDGE_ADDRESS}x" == "x" ]; then
-  BRIDGE_ADDRESS=172.21.1.1/24
-fi
-ip ad add ${BRIDGE_ADDRESS} dev virbr0
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-
-dnsmasq -i virbr0 -z -h --dhcp-range=172.21.1.10,172.21.1.250,4h
-dockerd --storage-driver=vfs --data-root=/data/docker/ &
-
-# Run CiscoIOUKeygen.py and log output
-echo "Running CiscoIOUKeygen.py..."
 python3 /data/CiscoIOUKeygen.py > /data/.iourc
 
-# Check if .iourc was generated and contains content
 if [ -s /data/.iourc ]; then
     echo ".iourc file generated successfully."
     cat /data/.iourc
@@ -43,7 +20,27 @@ if [ -s /data/iourc1 ]; then
     mv /data/iourc1 /data/.iourc
 else
     echo "Error: Failed to create iourc1 file."
+
+
+
+if [ "${CONFIG}x" == "x" ]; then
+	CONFIG=/data/config.ini
 fi
 
-# Start GNS3 server with the specified configuration
+if [ ! -e $CONFIG ]; then
+	cp /config.ini /data
+fi
+
+brctl addbr virbr0
+ip link set dev virbr0 up
+if [ "${BRIDGE_ADDRESS}x" == "x" ]; then
+  BRIDGE_ADDRESS=172.21.1.1/24
+fi
+ip ad add ${BRIDGE_ADDRESS} dev virbr0
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+dnsmasq -i virbr0 -z -h --dhcp-range=172.21.1.10,172.21.1.250,4h
+dockerd --storage-driver=vfs --data-root=/data/docker/ &
 gns3server -A --config $CONFIG
+
+
